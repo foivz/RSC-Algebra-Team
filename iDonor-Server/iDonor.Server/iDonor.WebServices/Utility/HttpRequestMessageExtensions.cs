e95750e9148b121a99db0a84ace1d7dca84eb0e4
Vedran Mandić <mandic.vedran@gmail.com>
2014-11-22 12:49:51 +0100

    Initial project setup.

---
+++ iDonor-Server/iDonor.Server/iDonor.WebServices/Utility/HttpRequestMessageExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+
+namespace iDonor.WebServices.Utility
+{
+    /// <summary>
+    /// Helper class with extensions over the HttpRequestMessage class
+    /// </summary>
+    public static class HttpRequestMessageExtensions
+    {
+        private const string HttpContext = "MS_HttpContext";
+        private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+
+
+        /// <summary>
+        /// Exposes the client IP address from the request object.
+        /// </summary>
+        /// <param name="request">A HttpRequestMessage.</param>
+        /// <returns>The IP address of the request sender.</returns>
+        public static string GetClientIpAddress(this HttpRequestMessage request)
+        {
+            if (request.Properties.ContainsKey(HttpContext))
+            {
+                dynamic ctx = request.Properties[HttpContext];
+                if (ctx != null)
+                {
+                    return ctx.Request.UserHostAddress;
+                }
+            }
+
+            if (request.Properties.ContainsKey(RemoteEndpointMessage))
+            {
+                dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+                if (remoteEndpoint != null)
+                {
+                    return remoteEndpoint.Address;
+                }
+            }
+
+            return null;
+        }
+    }
+}
